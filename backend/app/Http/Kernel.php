3dc7dfa62f56a59a19ee5527e324c858b2b814de
Mathieu JESER <bobdivx@gmail.com>
2026-08-23 20:13:32 +0200

    fix: allow SPA session auth on graft deploy-all (Sanctum stateful)

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -80,7 +80,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             ThrottleRequests::class.':api',
             SubstituteBindings::class,
         ],
